b7f8c14d98537a6a7649621ed4d21a1e28dbb835
GalinGdS <alcivan14lucas@gmail.com>
2023-09-23 18:43:44 -0300

    fica quieto Kick Buttowski

--- assets/calculadora.js
+++ assets/calculadora.js
@@ -12,7 +12,7 @@
             }
         }
 
-        // Adicione um evento de escuta ao select
+        // Adicione um evento de escuta ao select, e mostra opçẽs de traçoes de acordo com o metodo
         document.getElementById('opcoes-metodo').addEventListener('change', function() {
             var div1 = document.getElementById('div1');
             var div2 = document.getElementById('div2');
@@ -28,6 +28,9 @@
                 div2.style.display = 'none';
             }
         });
+
+
+
 
         // Preenche os inputs de acordo com o traço selecionado Método FCJ
         document.getElementById('traco-fcj').addEventListener('change', function() {
@@ -118,3 +121,31 @@
         MPA.value = '';
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtém o elemento select
+    var selectVolume = document.getElementById("select-volume");
+
+    // Obtém as divs
+    var volumeConhecido = document.getElementById("volume-conhecido");
+    var volumeDesconhecido = document.getElementById("volume-desconhecido");
+
+    // Adiciona um ouvinte de evento para o evento "change" no select
+    selectVolume.addEventListener("change", function() {
+        if (selectVolume.value === "volume-conhecido") {
+            // Se "Volume conhecido" for selecionado, mostra a div "volume-conhecido" e oculta a div "volume-desconhecido"
+            volumeConhecido.style.display = "block";
+            volumeDesconhecido.style.display = "none";
+        } else if (selectVolume.value === "volume-desconhecido") {
+            // Se "Volume desconhecido" for selecionado, mostra a div "volume-desconhecido" e oculta a div "volume-conhecido"
+            volumeConhecido.style.display = "none";
+            volumeDesconhecido.style.display = "block";
+        }
+    });
+    
+    // Define o valor inicial do select e exibe a div correspondente
+    selectVolume.value = "volume-conhecido";
+    volumeConhecido.style.display = "block";
+    volumeDesconhecido.style.display = "none";
+});
+
